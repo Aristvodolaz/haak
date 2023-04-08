b81176837fad92e75709d05a3e4864532b299336
prokopenko.a <kap.moral22@gmail.com>
2023-04-08 18:17:29 +0300

    create fragm on auth

---
+++ app/src/main/java/com/example/hakatonapp/fragment/AuthFragment.java
@@ -0,0 +1,54 @@
+package com.example.hakatonapp.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.hakatonapp.activity.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AuthFragment extends Fragment {
+
+    private FirebaseAuth auth;
+    public static AuthFragment newInstance() {
+        return new AuthFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        auth.signInWithEmailAndPassword().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isComplete()){
+                    Toast.makeText(getActivity(), "Good", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        auth.createUserWithEmailAndPassword().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Good", Toast.LENGTH_SHORT).show();
+                } else Toast.makeText(getActivity(), "Bad", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
